d8936ec4defb65259be62e534f554ca823559c4f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-09 14:18:39 +0800

    Improved code.

--- emver.js
+++ emver.js
@@ -34,6 +34,9 @@
 			"file": "emver.js",
 			"module": "emver",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/emver.git",
 			"test": "emver-test.js",
